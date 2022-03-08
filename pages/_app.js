6d0badd33d964e978b0c9aa24e8f93773dafa2f6
Kevin Cruz <kevin_mcp@outlook.com>
2022-03-07 23:19:49 -0300

    Finalizando com a tradução de inglés e espanhol

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
 
+import { I18nextProvider } from "react-i18next"
+import i18next from "i18next"
+import es from "./translations/es/global.json"
+import en from "./translations/en/global.json"
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  
+  //Lenguaje default
+  lng: "es",
+
+  //Indicar los lenguajes y rutas en las que se encuentran
+  resources: {
+    es: {
+      global: es
+    },
+    en: {
+      global: en
+    }
+  }
+})
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <I18nextProvider i18n={i18next}>
+      <Component {...pageProps} />
+    </I18nextProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+//Hook de i18n
+import { useTranslation } from "react-i18next"
 
 export default function Home() {
+
+  //t = text
+  const [t,i18n] = useTranslation("global")
+
   return (
     <>
-      <button>Español</button>
+      <button onClick={() => i18n.changeLanguage("es")}>Español</button>
       <button>Portuguese</button>
-      <button>Ingles</button>
+      <button onClick={() => i18n.changeLanguage("en")}>Ingles</button>
 
       <ul>
-        <li>Hola</li>
+        <li> {t("header.hello-world")}</li>
       </ul>
 
     </>
